4a3fd13c2bbcdda9f7b1bbdd332a4c4ad448ba30
Jrenatomt <jrenatomt@gmail.com>
2020-02-27 10:58:41 -0300

    Criado projeto

---
+++ src/main/java/com/projeto/demo/ProjetoSpring2Application.java
@@ -0,0 +1,13 @@
+package com.projeto.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpring2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpring2Application.class, args);
+	}
+
+}
