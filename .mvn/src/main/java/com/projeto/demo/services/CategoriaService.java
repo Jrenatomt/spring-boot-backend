8cf0347f17845ff16e3caf8a12d6fe45682fec3a
Jrenatomt <jrenatomt@gmail.com>
2020-04-15 11:55:47 -0300

    iniciando o projeto

---
+++ .mvn/src/main/java/com/projeto/demo/services/CategoriaService.java
@@ -0,0 +1,24 @@
+package com.projeto.demo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.projeto.demo.domain.Categoria;
+import com.projeto.demo.repositories.CategoriaRepository;
+import com.projeto.demo.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repository;
+
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	}
+
+}
---
+++ .mvn/src/main/java/com/projeto/demo/services/PedidoService.java
@@ -0,0 +1,24 @@
+package com.projeto.demo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.projeto.demo.domain.Pedido;
+import com.projeto.demo.repositories.PedidoRepository;
+import com.projeto.demo.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repository;
+
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+
+}
