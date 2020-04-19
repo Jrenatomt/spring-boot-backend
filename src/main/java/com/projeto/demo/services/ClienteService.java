e9500c9f40be88de1b2ad1495d6f0d6fcafb1861
Jrenatomt <jrenatomt@gmail.com>
2020-04-19 15:04:49 -0300

    inserindo novo cliente com post

--- src/main/java/com/projeto/demo/services/ClienteService.java
+++ src/main/java/com/projeto/demo/services/ClienteService.java
@@ -9,10 +9,16 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.projeto.demo.domain.Cidade;
 import com.projeto.demo.domain.Cliente;
+import com.projeto.demo.domain.Endereco;
+import com.projeto.demo.domain.enums.TipoCliente;
 import com.projeto.demo.dto.ClienteDTO;
+import com.projeto.demo.dto.ClienteNewDTO;
 import com.projeto.demo.repositories.ClienteRepository;
+import com.projeto.demo.repositories.EnderecoRepository;
 import com.projeto.demo.services.exceptions.DataIntegrityException;
 import com.projeto.demo.services.exceptions.ObjectNotFoundException;
 
@@ -21,11 +27,22 @@ public class ClienteService {
 
 	@Autowired
 	private ClienteRepository repository;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 
 	public Cliente find(Integer id) {
 		Optional<Cliente> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+	
+	@Transactional
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		obj = repository.save(obj);
+		enderecoRepository.saveAll(obj.getEnderecos());
+		return obj;
 	}
 	
 	public Cliente update(Cliente obj) {
@@ -40,7 +57,7 @@ public class ClienteService {
 		     repository.deleteById(id);
 		}
 		catch(DataIntegrityViolationException e) {
-			throw new DataIntegrityException("Não e possivel Excluir uma categoria que tem produtos.");
+			throw new DataIntegrityException("Não e possivel Excluir porque há entidades relacionadas.");
 		}
 	}
 	
@@ -58,11 +75,24 @@ public class ClienteService {
 		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
 	}
 	
+	public Cliente fromDTO(ClienteNewDTO objDto) {
+		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(), TipoCliente.toEnum(objDto.getTipo()));
+		Cidade cid = new Cidade(objDto.getCidadeId(), null, null);
+		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(), objDto.getBairro(), objDto.getCep(), cli, cid);
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDto.getTelefone1());
+		if(objDto.getTelefone2()!=null) {
+			cli.getTelefones().add(objDto.getTelefone2());
+		}
+		if(objDto.getTelefone3()!=null) {
+			cli.getTelefones().add(objDto.getTelefone3());
+		}
+		return cli;
+	}
+	
 	private void updateData(Cliente newObj, Cliente obj) {
 		newObj.setNome(obj.getNome());
 		newObj.setEmail(obj.getEmail());
 	
 	}
-
-
 }
