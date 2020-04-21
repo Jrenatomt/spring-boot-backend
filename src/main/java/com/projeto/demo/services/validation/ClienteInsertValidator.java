0298f72ebafba4404f024efe6033697395821ad3
Jrenatomt <jrenatomt@gmail.com>
2020-04-20 21:47:17 -0300

    Validação customizada: email nao pode repetir na inserção de cliente

--- src/main/java/com/projeto/demo/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/projeto/demo/services/validation/ClienteInsertValidator.java
@@ -6,13 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.projeto.demo.domain.Cliente;
 import com.projeto.demo.domain.enums.TipoCliente;
 import com.projeto.demo.dto.ClienteNewDTO;
+import com.projeto.demo.repositories.ClienteRepository;
 import com.projeto.demo.resources.exception.FieldMessage;
 import com.projeto.demo.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
 
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -26,6 +33,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		}
 		if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())){
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ invalido"));
+		}
+		
+		Cliente cli = clienteRepository.findByEmail(objDto.getEmail());
+		if(cli != null) {
+			list.add(new FieldMessage("email", "Email Existente"));
 		}
 			
 		
