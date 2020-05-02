7b40a31a413e8fb3682df63393f892ab83127bd4
Jrenatomt <jrenatomt@gmail.com>
2020-05-02 02:47:40 -0300

    perfil dev criado

---
+++ src/main/java/com/projeto/demo/config/DevConfig.java
@@ -0,0 +1,26 @@
+package com.projeto.demo.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.projeto.demo.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+	
+	@Autowired
+	private DBService dbService;
+	
+	@Bean
+	public boolean instantiateDataBase() throws ParseException {
+		dbService.instantiateTesteDateBase();
+		return true;
+	}
+	
+
+}
